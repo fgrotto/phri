54f8bab6ad09cef737533485fba6565330f24be1
Filippo <filippo.grotto@gmail.com>
2021-10-24 16:56:28 +0200

    Add basic implementation of 4 channel teleop (assignment 1)

--- four_channel_bilateral_teleoperation_architecture_params.m
+++ four_channel_bilateral_teleoperation_architecture_params.m
@@ -2,13 +2,25 @@
 clear all;
 close all;
 
+% Input function parameter (sin or step with low pass filter)
+A = 1;
+
+% Low pass frequency cuff off
+Fip = 10;
+% Sin frequency
+Fc = 1; 
+
 % Master controller
-Bm = 1;
-Km = 1;
+Bm = 0.2;
+Km = 15;
+
+% Human intention controller
+Ph = 1;
+Dh = 1;
 
 % Slave controller
-Bs = 1;
-Ks = 1;
+Bs = 0.2;
+Ks = 15;
 
 % Intertia of robot dynamics
 Mm = 0.5;
@@ -16,8 +28,10 @@ Ms = 2;
 
 % Make the real pole with re<0 to change the inertia of the robot
 % dynamics verify this value by adding them to the simulink model
-Dm = 5;
-Ds = 10;
+% Dm = 5;
+% Ds = 10;
+Dm = 0;
+Ds = 0;
 
 % Human impedance parameters
 Jh = 0.5;
@@ -28,4 +42,3 @@ Kh = 2000;
 Je = 0;
 Be = 100;
 Ke = 200;
-
